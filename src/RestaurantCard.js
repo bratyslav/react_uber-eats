a91a13dd278edadb01c40602afe91ed967e57482
bratyslav <beginner.altavista@gmail.com>
2019-08-26 19:11:13 +0300

    added home page component

--- src/RestaurantCard.js
+++ src/RestaurantCard.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import './RestaurantCard.css';
 
-const RestaurantCard = () => {
+const RestaurantCard = ({ shop }) => {
   return (
     <a href="#" className="restaurant-card__wrapper-link">
       <div className="restaurant-card">
         <img
-          src="https://d1ralsognjng37.cloudfront.net/ed032859-05fa-4761-9a4f-ac5fb7d5859a"
+          src={shop.heroImageUrl}
           alt="restaurant foto"
         />
 
         <div className="restaurant-card__restaurant-name">
-          <h3>To Be</h3>
+          <h3>{shop.title}</h3>
         </div>
 
         <div className="restaurant-card__type-of-dishes"> 
           <h4>
-            &#8372;&#8372; &middot; Японська &middot; Азiатська &middot; Сушi
+            {shop.categories.join(' · ')}
           </h4>  
         </div>
 
         <div className="restaurant-card__time-of-delivery">
-          <h5>25 - 35 Min</h5>
+          <h5>{shop.etaRange.text}</h5>
         </div>
       </div>
     </a>
